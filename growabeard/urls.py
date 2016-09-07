69284bea89a677c7f1749177f2fe886b8164e453
Jorge A. Torres <j@jorgetorres.co>
2016-09-06 23:37:12 -0500

    Add some new views Not being very specific because zZzzz

--- growabeard/urls.py
+++ growabeard/urls.py
@@ -13,16 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
-
 from . import views, settings
-
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index)
+    url(r'^', include('django.contrib.auth.urls')),
+    url(r'^$', views.index),
+    url(r'^upload/$', views.upload, name='upload')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- growabeard/views.py
+++ growabeard/views.py
@@ -1,4 +1,33 @@
 # encoding: utf-8
+from datetime import date, datetime
+from django.http import HttpResponseForbidden
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Campaign
+from .forms import UploadForm
+
 
 def index(request):
-    pass
+    return render(request, 'index.html')
+
+@login_required
+def upload(request):
+    campaign = Campaign.get_active()
+
+    if not campaign:
+        return HttpResponseForbidden()
+
+    today = date.today()
+
+    if campaign.entries.filter(user=request.user, created_at__date=today).exists():
+        messages.error(request, 'You\'ve already uploaded an image for today.')
+        return redirect('/')
+
+    form = UploadForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        campaign.entries.create(user=request.user, file=request.FILES['image'], created_at=datetime.now())
+        messages.success(request, 'Thanks for sharing your beard progress!')
+        return redirect('/')
+
+    return render(request, 'upload.html', dict(form=form))
