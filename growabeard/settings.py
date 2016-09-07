22a43636a670c3dc30288cad0836e8b0a4bb082a
Jorge A. Torres <j@jorgetorres.co>
2016-09-06 23:36:25 -0500

    Add settings for login and logout URLs

--- growabeard/settings.py
+++ growabeard/settings.py
@@ -119,9 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 MEDIA_URL = '/uploads/'
+
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = '/'
