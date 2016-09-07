de21b48059d9333a33932bfe24afdc7dbae33635
Willington Vega <wvega@wvega.com>
2016-09-07 14:23:35 -0500

    Add STATIC_ROOT setting.
    Required for `python manage.py collectstatic` to work.

--- growabeard/settings.py
+++ growabeard/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
