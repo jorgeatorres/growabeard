c8115b5de59bdc8f38a31487d3a61afb9f085a8e
Jorge A. Torres <j@jorgetorres.co>
2016-09-06 23:36:00 -0500

    Add method to obtain current campaign

--- growabeard/models.py
+++ growabeard/models.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 
 class Campaign(models.Model):
@@ -11,9 +12,14 @@ class Campaign(models.Model):
     def __str__(self):
         return '%s (%s - %s)' % (self.title, self.start_date, self.end_date)
 
+    @classmethod
+    def get_active(cls):
+        today = datetime.date.today()
+        return cls.objects.filter(start_date__lte=today, end_date__gte=today).first() # FIXME: add support for several campaigns simultaneously
+
 
 class CampaignEntry(models.Model):
-    campaign = models.ForeignKey('Campaign', on_delete=models.CASCADE)
+    campaign = models.ForeignKey('Campaign', related_name='entries', on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     file = models.ImageField(upload_to='')
     created_at = models.DateTimeField()
